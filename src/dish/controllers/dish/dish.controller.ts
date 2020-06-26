1e1667130d3151362757bca82c204e389ce72262
tikimcrzx <tikimioo723@gmail.com>
2020-06-26 01:42:36 -0700

    [Add dish module]

--- src/dish/controllers/dish/dish.controller.ts
+++ src/dish/controllers/dish/dish.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Res,
+  HttpStatus,
+  Get,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { DishService } from '../../services/dish/dish.service';
+import { CreateDishDTO, UpdateDishDTO } from '../../input-dto';
+import { Response } from 'express';
 
 @Controller('dish')
-export class DishController {}
+export class DishController {
+  constructor(private readonly _dishService: DishService) {}
+
+  @Post()
+  async create(@Body() createDishDto: CreateDishDTO, @Res() res: Response) {
+    const createdDish = await this._dishService.create(createDishDto);
+    res.status(HttpStatus.CREATED).json(createdDish);
+  }
+
+  @Get()
+  async findAll(@Res() res: Response) {
+    const dishes = await this._dishService.findAll();
+    res.status(HttpStatus.OK).json(dishes);
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string, @Res() res: Response) {
+    const dish = await this._dishService.findById(id);
+    res.status(HttpStatus.OK).json(dish);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateDishDto: UpdateDishDTO,
+    @Res() res: Response,
+  ) {
+    const updatedDish = await this._dishService.update(id, updateDishDto);
+    res.status(HttpStatus.OK).json(updatedDish);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string, @Res() res: Response) {
+    const deletedDish = await this._dishService.delete(id);
+    res.status(HttpStatus.OK).json(deletedDish);
+  }
+}
--- src/dish/services/dish/dish.service.ts
+++ src/dish/services/dish/dish.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Dish } from '../../schemas';
+import { CreateDishDTO, UpdateDishDTO } from '../../input-dto';
 
 @Injectable()
-export class DishService {}
+export class DishService {
+  constructor(@InjectModel('Dish') private readonly _dishModel: Model<Dish>) {}
+
+  async create(createDishDto: CreateDishDTO): Promise<Dish> {
+    let dish: Dish;
+
+    dish = await this._dishModel.findOne({ name: createDishDto.name });
+
+    if (dish) throw new NotFoundException(`dish already exists`);
+    else dish = await this._dishModel.create(createDishDto);
+
+    return dish;
+  }
+
+  async findAll(): Promise<Dish[]> {
+    return await this._dishModel.find().exec();
+  }
+
+  async findById(_id: string): Promise<Dish> {
+    const dish: Dish = await this._dishModel.findById(_id);
+    if (!dish) throw new NotFoundException(`dish id = ${_id} not found`);
+    return dish;
+  }
+
+  async update(_id: string, updateDishDto: UpdateDishDTO): Promise<Dish> {
+    const updatedDish: Dish = await this._dishModel.findById(_id);
+
+    if (!updatedDish) throw new NotFoundException(`dish id = ${_id} not found`);
+
+    updatedDish.details = updateDishDto.details;
+    updatedDish.image = updateDishDto.image;
+    updatedDish.ingredients = updateDishDto.ingredients;
+    updatedDish.name = updateDishDto.name;
+
+    updatedDish.save();
+
+    return updatedDish;
+  }
+
+  async delete(_id: string): Promise<boolean> {
+    const deletedDish: Dish = await this._dishModel.findById(_id);
+    if (!deletedDish) throw new NotFoundException(`dish id = ${_id} not found`);
+    deletedDish.remove();
+    return true;
+  }
+}
